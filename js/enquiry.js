d0661f74c21ae7d4a90b349169b2d7438fdd60e4
Rajiv S. Raghavan <rajivsr@yahoo.com>
2016-02-26 17:14:03 -0500

    Added Contact Form for Enquiry Button

--- js/enquiry.js
+++ js/enquiry.js
@@ -1,8 +1,27 @@
+/*JQuery Function for Enquiry Button*/
 $(document).ready(function(){
 	$('#enquirybutton').mouseenter(function(){
 		$('#enquirybutton').fadeTo("fast",1);
 		$('#enquirybutton').mouseleave(function(){
-			$('#enquirybutton').fadeTo("fast",0.3)
-			});
+			$('#enquirybutton').fadeTo("fast",0.3);
 		});
+	});
 });
+
+/*JQuery Show Contact Form when Enquiry Button is clicked*/
+$(document).ready(function(){
+	$('#enquirybutton').click(function(){
+		$('#contactform').show();
+	});
+	
+});
+
+/*JQuery Hide Contact Form when cross button is clicked */
+$(document).ready(function(){
+	$('#closeform').click(function(){
+		$('#contactform').hide();
+	});
+	
+});
+
+
