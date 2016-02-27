9ae0bf10a358d5622c2e9a778e89375c08ce14c3
Rajiv S. Raghavan <rajivsr@yahoo.com>
2016-02-26 19:52:20 -0500

    Added variable body

--- php/contact_form_handler.php
+++ php/contact_form_handler.php
@@ -2,9 +2,10 @@
 	$name=$_POST['name'];
 	$email_address = $_POST['email'];
 	$subject='Civic Steel Replies';
-	$message = "You have received a new message. ".
+	$message=$_POST['msg'];
+	$body = "You have received a new message. ".
 	" Here are the details:\n Name: $name \n ".
-	"Email: $email_address\n Message \n $_POST['msg']";
+	"Email: $email_address\n Message: $message \n ";
 	
-	mail('rajivsr2309@gmail.com', $subject, $message);
+	mail('rajivsr2309@gmail.com', $subject, $body);
 ?> 
