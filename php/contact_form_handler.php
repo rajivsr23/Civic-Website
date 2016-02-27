f3da7624497de015a00cdc0ef217150441ec9ed0
Rajiv S. Raghavan <rajivsr@yahoo.com>
2016-02-26 19:47:36 -0500

    Modified the Body of the mail

--- php/contact_form_handler.php
+++ php/contact_form_handler.php
@@ -1,7 +1,10 @@
 <?php
-$to = $_POST['email'];
-$subject='Civic Steel Replies';
-$message = $_POST['msg'];
-
-mail($to, $subject, $message);
+	$name=$_POST['name'];
+	$email_address = $_POST['email'];
+	$subject='Civic Steel Replies';
+	$message = "You have received a new message. ".
+	" Here are the details:\n Name: $name \n ".
+	"Email: $email_address\n Message \n $_POST['msg']";
+	
+	mail('rajivsr2309@gmail.com', $subject, $message);
 ?> 
