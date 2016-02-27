cfc793170411c02d7af4a239dfec5f6f00aac6d9
Rajiv S. Raghavan <rajivsr@yahoo.com>
2016-02-26 20:59:57 -0500

    Removed Validation: Only Header

--- php/contact_form_handler.php
+++ php/contact_form_handler.php
@@ -10,15 +10,8 @@
 	"Email: $email_address\n Phone Number: $phone \n Message: $message \n ";
 	
 	mail('rajivsr2309@gmail.com,rajivsr5462@gmail.com', $subject, $body);
-	$url="www.civicsteel.com";
+	$url="/index.php";
+	header( "Location: $url" );
 	
-	if(@mail($emailRecipient, $subject, $message, $headers))
-	{
-		$message = "Message Sent Successfully";
-		echo "<script type='text/javascript'>alert('$message');</script>";
-		header( "Location: $url" );
-		}else{
-		echo "Mail Not Sent";
-		header( "Location: $url" );
-	}
+}
 ?> 
